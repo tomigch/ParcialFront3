bb38af41f1ac0c93f769e8890acc71d292be4a22
tomigch <tomasgchiappealt@gmail.com>
2023-03-07 14:23:46 -0300

    preparando el parcial

---
+++ GonzalezChiappe_Tomas_Parcial/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import Card from "./Card";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+
+export default App;
